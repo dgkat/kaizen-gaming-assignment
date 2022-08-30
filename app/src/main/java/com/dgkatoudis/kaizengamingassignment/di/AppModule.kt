fb9a5ed0552b10a1c960e541c2359ad0a1bcaf0a
dgkat <dgkatoudis@gmail.com>
2022-08-30 18:43:24 +0300

    Provide Hilt dependencies

--- app/src/main/java/com/dgkatoudis/kaizengamingassignment/di/AppModule.kt
+++ app/src/main/java/com/dgkatoudis/kaizengamingassignment/di/AppModule.kt
@@ -1,4 +1,41 @@
 package com.dgkatoudis.kaizengamingassignment.di
 
-class AppModule {
+import com.dgkatoudis.kaizengamingassignment.data.repository.SportRepositoryImpl
+import com.dgkatoudis.kaizengamingassignment.domain.repository.SportRepository
+import com.dgkatoudis.kaizengamingassignment.domain.usecases.GetSportsWithEvents
+import com.dgkatoudis.kaizengamingassignment.presentation.sports.DateFormatter
+import com.dgkatoudis.kaizengamingassignment.presentation.sports.SportsEventMapper
+import com.dgkatoudis.kaizengamingassignment.presentation.sports.SportsMapper
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    fun provideDateFormatter():DateFormatter{
+        return DateFormatter()
+    }
+
+    @Provides
+    fun provideSportsEventMapper(dateFormatter: DateFormatter): SportsEventMapper {
+        return SportsEventMapper(dateFormatter)
+    }
+
+    @Provides
+    fun provideSportsMapper(sportsEventMapper: SportsEventMapper):SportsMapper{
+        return SportsMapper(sportsEventMapper)
+    }
+
+    @Provides
+    fun provideGetSportsWithEvents(repository: SportRepository):GetSportsWithEvents{
+        return GetSportsWithEvents(repository)
+    }
+    @Provides
+    fun provideSportsRepository():SportRepository{
+        return SportRepositoryImpl()
+    }
 }
