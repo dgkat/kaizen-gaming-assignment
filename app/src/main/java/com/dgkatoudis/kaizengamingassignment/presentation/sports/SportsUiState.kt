70973f39a1cfb0fd02f7e908909609779406c666
dgkat <katoudis@thinkdesquared.com>
2023-09-24 15:48:35 +0300

    formatting: remove comments

--- app/src/main/java/com/dgkatoudis/kaizengamingassignment/presentation/sports/SportsUiState.kt
+++ app/src/main/java/com/dgkatoudis/kaizengamingassignment/presentation/sports/SportsUiState.kt
@@ -1,9 +1,5 @@
 package com.dgkatoudis.kaizengamingassignment.presentation.sports
 
-/*data class SportsUiState(
-    val data: List<UiSport> = emptyList()
-)*/
-
-enum class SportsUiState{
-    SUCCESS,LOADING,ERROR
+enum class SportsUiState {
+    SUCCESS, LOADING, ERROR
 }
